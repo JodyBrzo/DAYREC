98e6cbf057219d8767e853905e79bd42ca85a365
Carly Peyton <carlypeyton@gmail.com>
2021-03-22 18:54:26 -0400

    Update server with middleware for authentication, used in handlebars navbar partial.

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+//Middleware for user authentication
 app.use((req, res, next) => {
   res.locals.login = req.isAuthenticated();
   next();
