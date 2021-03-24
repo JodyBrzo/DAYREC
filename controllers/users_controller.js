fbde252641d21a735dd0b169d4bd1803f0f13276
Jody Brzovski <jody.brzovski@gmail.com>
2021-03-24 15:49:58 -0400

    Completed all functionality for Server Side Routes

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,34 +1,46 @@
-const express = require("express");
-const router = express.Router();
 const passport = require("../config/passport");
 const userRepo = require("../repositories/userRepository");
 
-module.exports = () => {
+module.exports = app => {
   // when user logs on we send you back the student name
-  router.post("/api/login", passport.authenticate("local"), (req, res) => {
+  app.post("/api/login", passport.authenticate("local"), (req, res) => {
     res.json({ studentName: req.user.studentName });
   });
 
   //when a user signs up we create the user and send back studentName
-  router.post("/api/signup", (req, res) => {
-    const userDb = userRepo.createUser(
-      req.body.studentName,
-      req.body.email,
-      req.body.password
-    );
-
-    res.json({ studentName: userDb.studentName });
+  app.post("/api/signup", (req, res) => {
+    userRepo
+      .createUser(req.body.studentName, req.body.email, req.body.password)
+      .then(user => {
+        res.json({ studentName: userDb.studentName });
+        console.log(user);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
 
   //get 1 user - not in use at the moment
-  router.get("/api/user", (req, res) => {
-    const user = userRepo.getUser(req.query.userId);
-    res.json(user);
+  app.get("/api/user", (req, res) => {
+    userRepo
+      .getUser(req.query.userId)
+      .then(user => {
+        res.json(user);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
 
   //get all users - not in use at the moment
-  router.get("/api/users", (req, res) => {
-    const users = userRepo.getUsers();
-    res.json(users);
+  app.get("/api/users", (req, res) => {
+    userRepo
+      .getUsers()
+      .then(users => {
+        res.json(users);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   });
 };
--- server.js
+++ server.js
@@ -3,18 +3,13 @@ global.systemStatus = undefined;
 // Requiring necessary npm package
 const express = require("express");
 const session = require("express-session");
-const exphbs = require("express-handlebars");
+const handlebars = require("express-handlebars");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
-const path = require("path");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8083;
 const db = require("./models");
-
-// Import routes and give the server access to them.
-const routes = require('./controllers');
-
 
 const systemStatusRepo = require("./repositories/systemStatusRepository");
 global.systemStatus = systemStatusRepo.getAllowBetStatus();
@@ -24,52 +19,40 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-//Middleware for user authentication
-app.use((req, res, next) => {
-  res.locals.user = req.user;
-  res.locals.login = req.isAuthenticated();
-  next();
-});
-
-// Delete two lines below if not needed
-require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-
-// Handlebars views
-app.set("views", path.join(__dirname, "views"));
-app.engine(
-  "handlebars",
-  exphbs.create({
-    defaultLayout: "main",
-    layoutsDir: app.get("views") + "/layouts",
-    partialsDir: app.get("views") + "/partials"
-  }).engine
-);
-
 
 // We need to use sessions to keep track of our user's login status
 app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: false })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Import controllerss
+require("./controllers/admin_controller.js")(app);
+require("./controllers/bets_controller.js")(app);
+require("./controllers/users_controller.js")(app);
 
-//Set Handlebars
+//Sets our app to use the handlebars engine
+app.set("view engine", "handlebars");
+
+// Setup Handlebars Engine
+app.engine(
+  "handlebars",
+  handlebars({
+    layoutsDir: __dirname + "/views/layouts",
+    partialsDir: __dirname + "/views/partials",
+    extname: "handlebars",
+    defaultLayout: "main"
+  })
+);
+
+app.use((req, res, next) => {
+  res.locals.isNotRoot = req.url !== "/";
+  next();
+});
 
 // Requiring our routes
-//SHOULDN"T THIS JUST BE FOR THE CONTROLLER? SHOULD LOOK LIKE THIS?
-// require("./controllers/bets_controller.js")(app);
-// require("./controllers/recordlogs_controller.js")(app);
-// require("./controllers/users_controller.js")(app);
-//OR
-// require("./controllers/")(app);
-
 require("./routes/html-routes.js")(app);
-require("./routes/api-routes.js")(app);
-// require("./routes/user-route.js")(app);
-
-//Set Handlebars
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
