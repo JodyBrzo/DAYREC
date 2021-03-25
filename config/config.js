c07b438d0fe093ac87987817c3e6d694b9efbb59
Jody Brzovski <jody.brzovski@gmail.com>
2021-03-24 21:00:49 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   development: {
     username: "root",
-    password: process.env.DB_Password,
+    password: "snowPlants45",
     database: "dayrec_db",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   test: {
     username: "root",
-    password: process.env.DB_Password,
+    password: "snowPlants45",
     database: "dayrec_db",
     host: "127.0.0.1",
     dialect: "mysql"
